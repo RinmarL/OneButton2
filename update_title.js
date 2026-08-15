29e1689ca04a24cfb380d7589696046c66345796
aorinrin1 <aorinrin1@users.noreply.github.com>
2026-08-15 19:16:21 +0900

    Update AICHANGELOG and clean temporary files

--- update_title.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('index.html', 'utf-8');
-
-content = content.replace('<title>FACTORY ESCAPE</title>', '<title>OneButton</title>');
-
-fs.writeFileSync('index.html', content, 'utf-8');
-console.log('Title updated to OneButton');
